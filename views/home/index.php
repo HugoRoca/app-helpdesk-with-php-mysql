0b6dfbd1d2900b28b4de1601e2ae8c518f1f6769
hugoRoca <hugo.rock20@hotmail.com>
2021-09-11 00:09:54 -0500

    part 1 - configs views container

---
+++ views/home/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+
+  <?php require_once("../shared/head.php") ?>
+  <title>HelpDesk App</title>
+</head>
+
+<body class="with-side-menu">
+
+  <?php require_once("../shared/header.php") ?>
+
+  <div class="mobile-menu-left-overlay"></div>
+  
+  <?php require_once("../shared/nav.php") ?>
+
+  <div class="page-content">
+    <div class="container-fluid">
+      Blank page.
+    </div>
+    <!--.container-fluid-->
+  </div>
+  <!--.page-content-->
+
+  <?php require_once("../shared/scripts.php") ?>
+</body>
+
+</html>
---
+++ views/shared/nav.php
@@ -0,0 +1,48 @@
+<nav class="side-menu">
+  <ul class="side-menu-list">
+    <li class="red">
+      <a href="mail.html">
+        <i class="font-icon glyphicon glyphicon-send"></i>
+        <span class="lbl">Mail</span>
+      </a>
+    </li>
+    <li class="blue-dirty">
+      <a href="tables.html">
+        <span class="glyphicon glyphicon-th"></span>
+        <span class="lbl">Tables</span>
+      </a>
+    </li>
+    <!-- <header class="side-menu-title">Tags</header> -->
+    <li>
+      <a href="#">
+        <i class="tag-color green"></i>
+        <span class="lbl">Website</span>
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        <i class="tag-color grey-blue"></i>
+        <span class="lbl">Bugs/Errors</span>
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        <i class="tag-color red"></i>
+        <span class="lbl">General Problem</span>
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        <i class="tag-color pink"></i>
+        <span class="lbl">Questions</span>
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        <i class="tag-color orange"></i>
+        <span class="lbl">Ideas</span>
+      </a>
+    </li>
+  </ul>
+</nav>
+<!--.side-menu-->
