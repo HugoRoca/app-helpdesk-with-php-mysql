88daf5f9333e7c6e7df9cb35139a30fa0cd7eeef
hugoRoca <hugo.rock20@hotmail.com>
2021-11-12 23:35:36 -0500

    View by idRol

--- views/shared/nav.php
+++ views/shared/nav.php
@@ -1,48 +1,46 @@
 <nav class="side-menu">
-  <ul class="side-menu-list">
-    <li class="red">
-      <a href="mail.html">
-        <i class="font-icon glyphicon glyphicon-send"></i>
-        <span class="lbl">Dashboard</span>
-      </a>
-    </li>
-    <li class="blue-dirty">
-      <a href="..\ticket\">
-        <span class="glyphicon glyphicon-th"></span>
-        <span class="lbl">Tickets</span>
-      </a>
-    </li>
-    <!-- <header class="side-menu-title">Tags</header> -->
-    <li>
-      <a href="../ticket/query.php">
-        <i class="glyphicon glyphicon-th"></i>
-        <span class="lbl">Consultant Ticket</span>
-      </a>
-    </li>
-    <!-- <li>
-      <a href="#">
-        <i class="tag-color grey-blue"></i>
-        <span class="lbl">Bugs/Errors</span>
-      </a>
-    </li>
-    <li>
-      <a href="#">
-        <i class="tag-color red"></i>
-        <span class="lbl">General Problem</span>
-      </a>
-    </li>
-    <li>
-      <a href="#">
-        <i class="tag-color pink"></i>
-        <span class="lbl">Questions</span>
-      </a>
-    </li>
-    <li>
-      <a href="#">
-        <i class="tag-color orange"></i>
-        <span class="lbl">Ideas</span>
-      </a>
-    </li> -->
-  </ul>
+  <?php
+  if ($_SESSION["idRol"] == 1) {
+  ?>
+    <ul class="side-menu-list">
+      <li class="red">
+        <a href="mail.html">
+          <i class="font-icon glyphicon glyphicon-send"></i>
+          <span class="lbl">Dashboard</span>
+        </a>
+      </li>
+      <li class="blue-dirty">
+        <a href="..\ticket\">
+          <span class="glyphicon glyphicon-th"></span>
+          <span class="lbl">Tickets</span>
+        </a>
+      </li>
+      <!-- <header class="side-menu-title">Tags</header> -->
+      <li>
+        <a href="../ticket/query.php">
+          <i class="glyphicon glyphicon-th"></i>
+          <span class="lbl">Consultant Ticket</span>
+        </a>
+      </li>
+    </ul>
+  <?php
+  } else {
+  ?>
+    <ul class="side-menu-list">
+      <li class="red">
+        <a href="mail.html">
+          <i class="font-icon glyphicon glyphicon-send"></i>
+          <span class="lbl">Dashboard</span>
+        </a>
+      </li>
+      <li>
+        <a href="../ticket/query.php">
+          <i class="glyphicon glyphicon-th"></i>
+          <span class="lbl">Consultant Ticket</span>
+        </a>
+      </li>
+    </ul>
+  <?php
+  }
+  ?>
 </nav>
-<!--.side-menu-->
